2fe66006a4fc8211dbb124f523ca0040a4c4684f
RuoyiDu <61931443+RuoyiDu@users.noreply.github.com>
2020-05-14 07:47:06 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,16 +44,16 @@ def jigsaw_generator(images, n):
     for a in range(n):
         for b in range(n):
             l.append([a, b])
-    halfsize = 448 // n
+    block_size = 448 // n
     rounds = n ** 2
     random.shuffle(l)
     jigsaws = images.clone()
     for i in range(rounds):
         x, y = l[i]
-        temp = jigsaws[..., 0:halfsize, 0:halfsize].clone()
-        jigsaws[..., 0:halfsize, 0:halfsize] = jigsaws[..., x * halfsize:(x + 1) * halfsize,
-                                                y * halfsize:(y + 1) * halfsize].clone()
-        jigsaws[..., x * halfsize:(x + 1) * halfsize, y * halfsize:(y + 1) * halfsize] = temp
+        temp = jigsaws[..., 0:block_size, 0:block_size].clone()
+        jigsaws[..., 0:block_size, 0:block_size] = jigsaws[..., x * block_size:(x + 1) * block_size,
+                                                y * block_size:(y + 1) * block_size].clone()
+        jigsaws[..., x * block_size:(x + 1) * block_size, y * block_size:(y + 1) * block_size] = temp
 
     return jigsaws
 
